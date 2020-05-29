dd75ce88e36054fc456916726affad6837ba32f7
evilwombat <evilwombat@peregrine.local>
2020-05-29 02:43:22 -0700

    Countdown!
    Signed-off-by: evilwombat <evilwombat@peregrine.local>

--- Src/gfx_util.c
+++ Src/gfx_util.c
@@ -3,55 +3,108 @@
 #include "lcd.h"
 #include "util.h"
 
-const static struct {
-    uint8_t foreground[8];
-    uint8_t shadow[8]
-} digits[10] = {
-    {
-        {0b11111110,
-         0b10000110,
-         0b10001010,
-         0b10010010,
-         0b10100010,
-         0b11000010,
-         0b11111110,
-         0b00000000},
+const static uint8_t digit_data[10 * 7] = {
+    /* 0 */
+    0b1111111,
+    0b1000011,
+    0b1000101,
+    0b1001001,
+    0b1010001,
+    0b1100001,
+    0b1111111,
 
-        {0b00000000,
-         0b01111001,
-         0b01000001,
-         0b01000101,
-         0b01001001,
-         0b01010001,
-         0b00000001,
-         0b01111111},
-    },
-    {
-        {0b00010000,
-         0b00110000,
-         0b00010000,
-         0b00010000,
-         0b00010000,
-         0b00010000,
-         0b01111100},
+    /* 1 */
+    0b0001000,
+    0b0011000,
+    0b0001000,
+    0b0001000,
+    0b0001000,
+    0b0001000,
+    0b0001000,
 
 
-        {0b00000000,
-         0b00000000,
-         0b00000000,
-         0b00000000,
-         0b00000000,
-         0b00000000,
-         0b00000000},
-    },
+    /* 2 */
+    0b1111111,
+    0b1000001,
+    0b0000001,
+    0b1111111,
+    0b1000000,
+    0b1000000,
+    0b1111111,
+
+    /* 3 */
+    0b1111111,
+    0b1000001,
+    0b0000001,
+    0b0011111,
+    0b0000001,
+    0b1000001,
+    0b1111111,
+
+    /* 4 */
+    0b1000000,
+    0b1000000,
+    0b1000100,
+    0b1000100,
+    0b1111111,
+    0b0000100,
+    0b0000100,
+
+
+    /* 5 */
+    0b1111111,
+    0b1000000,
+    0b1000000,
+    0b1111111,
+    0b0000001,
+    0b1000001,
+    0b1111111,
+
+    /* 6 */
+    0b1111111,
+    0b1000000,
+    0b1000000,
+    0b1111111,
+    0b1000001,
+    0b1000001,
+    0b1111111,
+
+    /* 7 */
+    0b1111111,
+    0b0000001,
+    0b0000001,
+    0b0000010,
+    0b0000100,
+    0b0001000,
+    0b0010000,
+
+    /* 8 */
+    0b1111111,
+    0b1000001,
+    0b1000001,
+    0b1111111,
+    0b1000001,
+    0b1000001,
+    0b1111111,
+
+    /* 9 */
+    0b1111111,
+    0b1000001,
+    0b1000001,
+    0b1111111,
+    0b0000001,
+    0b0000001,
+    0b1111111,
 };
 
 static void draw_block(int x, int y, int scale, int shadow)
 {
     int i, j;
+    int value = 0;
 
     for (i = 0; i < scale; i++) {
         for (j = 0; j < scale; j++) {
+
 
             if (!shadow) {
                 lcd_pset(x + i, y + j, 1);
@@ -67,27 +120,40 @@ static void draw_block(int x, int y, int scale, int shadow)
 void gfx_draw_big_digit(int x, int y, int scale, int digit)
 {
     int i, j;
+    int offset = digit * 7;
 
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-
-            if (digits[digit].shadow[j] & BIT(7 - i)) {
-                draw_block(x + i * scale, y + j * scale, scale, 1);
-            }
-
-            if (digits[digit].foreground[j] & BIT(7 - i)) {
+    for (i = 0; i < 7; i++) {
+        for (j = 0; j < 7; j++) {
+            if (digit_data[offset + j] & BIT(6 - i)) {
+                draw_block(x + (i + 1) * scale, y + (j + 1) * scale, scale, 1);
                 draw_block(x + i * scale, y + j * scale, scale, 0);
             }
         }
     }
 }
 
+void gfx_draw_countdown(int value)
+{
+    int x = 20;
+    int y = 10;
+    int scale = 4;
+
+    gfx_draw_big_digit(x, y, scale, value / 10);
+    gfx_draw_big_digit(x + scale * 9, y, scale, value % 10);
+}
+
 void gfx_test()
 {
     con_printf("Hello GFX\n");
 
-    gfx_draw_big_digit(21, 21, 3, 0);
-    lcd_update();
+    while(1) {
+        for (int i = 0; i < 100; i++) {
+            lcd_clear();
+            gfx_draw_countdown(99 - i);
+            lcd_update();
+            HAL_Delay(1000);
+        }
+    }
 
     while(1);
 }
